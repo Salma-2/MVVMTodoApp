225d7a368cdf66cebbc8760b1d5a33e5fa75d8fc
= <=>
2022-10-24 10:53:29 +0200

    Implemented screens with their vm

---
+++ app/src/main/java/com/salma/mvvmtodoapp/ui/add_edit_todo/AddEditTodoViewModel.kt
@@ -0,0 +1,87 @@
+package com.salma.mvvmtodoapp.ui.add_edit_todo
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.salma.mvvmtodoapp.data.Todo
+import com.salma.mvvmtodoapp.data.TodoRepository
+import com.salma.mvvmtodoapp.util.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import com.salma.mvvmtodoapp.ui.add_edit_todo.AddEditTodoEvent.*
+
+@HiltViewModel
+class AddEditTodoViewModel @Inject constructor(
+    private val repository: TodoRepository,
+    savedStateHandle: SavedStateHandle, // to restore vm state & for navigation args
+) : ViewModel() {
+    var todo by mutableStateOf<Todo?>(null)
+        private set
+
+    var title by mutableStateOf("")
+        private set
+
+    var description by mutableStateOf("")
+        private set
+
+
+    // channel for one time event
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    init {
+        val todoId = savedStateHandle.get<Int>("todoId")!!
+        if (todoId != -1) {
+            viewModelScope.launch {
+                repository.getTodoById(todoId)?.let { todo ->
+                    title = todo.title
+                    description = todo.description ?: ""
+                    this@AddEditTodoViewModel.todo = todo
+                }
+            }
+        }
+    }
+
+    fun onEvent(event: AddEditTodoEvent) {
+        when (event) {
+            is OnSaveTodoClick -> {
+                viewModelScope.launch {
+                    if (title.isNotBlank()) {
+                        sendUiEvent(UiEvent.ShowSnackbar(
+                            "The title can not be empty "
+                        ))
+                        return@launch
+                    }
+                    repository.insertTodo(
+                        Todo(
+                            title = title,
+                            description = description,
+                            isDone = todo?.isDone ?: false,
+                            id = todo?.id
+                        )
+                    )
+                    sendUiEvent(UiEvent.PopBackStack)
+                }
+            }
+            is OnDescriptionChange -> {
+                description = event.description
+            }
+            is OnTitleChange -> {
+                title = event.title
+
+            }
+        }
+    }
+
+    private fun sendUiEvent(event: UiEvent) {
+        viewModelScope.launch {
+            _uiEvent.send(event)
+        }
+    }
+}
---
+++ app/src/main/java/com/salma/mvvmtodoapp/ui/todo_list/TodoItem.kt
@@ -0,0 +1,71 @@
+package com.salma.mvvmtodoapp.ui.todo_list
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.salma.mvvmtodoapp.data.Todo
+
+@Composable
+fun TodoItem(
+    todo: Todo,
+    onEvent: (TodoListEvent) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = todo.title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                IconButton(onClick = {
+                    onEvent(TodoListEvent.OnDeleteTodoClick(todo))
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete")
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            todo.description?.let { description ->
+                Text(
+                    text = description,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Light
+                )
+            }
+
+            Checkbox(
+                checked = todo.isDone,
+                onCheckedChange = { isChecked ->
+                    onEvent(
+                        TodoListEvent.OnDoneChange(
+                            todo,
+                            isChecked
+                        ))
+                })
+        }
+    }
+}
